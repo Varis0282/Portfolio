4f97372adbffe889117524ee8063445f9b177e7c
Varis Rajak <109987290+Varis0282@users.noreply.github.com>
2023-12-03 03:10:47 +0530

    Deploy changes

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ const dbconfig = require('./dbconfig');
 const port = process.env.PORT || 5000 ;
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname,"front-end/build")));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, "front-end/build/index.html"));
-    });
-}
-
 require('./Models/user-model');
 require('./Models/portfolio-model');
 
 app.use(express.json());
 app.use(require('./Routes/portfolioRoutes'));
+
+// deployment config
+const path = require("path");
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/front-end/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "front-end", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log('App listening on port '+ port);
